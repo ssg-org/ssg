13887a09dbe8c8f2b59d4eac80761e204b51d7b4
Eljazovic Samir <samir@atlantbh.com>
2012-06-18 23:37:35 +0200

    Adding support for filters on issues#index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    def clear_params (new_map)
+        cleared = params.clone
+        cleared.delete(:action)       
+        cleared.delete(:controller)
+        return cleared.merge(new_map)
+    end
 end
