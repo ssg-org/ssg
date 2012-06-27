8b690274632886c339af658a49f853202bee2f05
Eljazovic Samir <samir@atlantbh.com>
2012-06-27 15:21:57 +0200

    Set locale=bs as default

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
 
  	def set_locale
-  		I18n.locale = I18n.available_locales.include?(params[:locale].to_sym) ? params[:locale] : I18n.default_locale
+ 		if (params[:locale].nil? || !I18n.available_locales.include?(params[:locale].to_sym))
+  			I18n.locale = I18n.default_locale
+  		else
+  			I18n.locale = params[:locale]
+  		end
 	end
 end
