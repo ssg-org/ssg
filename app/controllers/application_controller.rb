d80fcc708e068b99f1d7cc09a4a8482f2e6d9742
Eljazovic Samir <samir@atlantbh.com>
2012-06-27 15:15:50 +0200

    Check if passed local is valid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
 
  	def set_locale
-  		I18n.locale = params[:locale] || I18n.default_locale
+  		I18n.locale = I18n.available_locales.include?(params[:locale].to_sym) ? params[:locale] : I18n.default_locale
 	end
 end
