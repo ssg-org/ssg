7813cf335c40bf51c3b6e636a6bdb2ac7baeea52
Eljazovic Samir <samir.eljazovic@gmail.com>
2012-10-05 23:27:32 +0200

    Adding HTTP authentication when app is running in prod mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  # Only run this filter if in production mode, as don't want to enter password in development
+  before_filter :check_access if Rails.env == "production"
   before_filter :check_login
-  
+
   def check_login
     if (session[:id])
       @user = User.find_by_id(session[:id])
@@ -15,6 +17,14 @@ class ApplicationController < ActionController::Base
   
   def self.disable_layout_for_ajax(layout_name = 'application')
     layout Proc.new { |controller| controller.request.xhr? ? nil : layout_name }    
-  end  
+  end
+
+  private
+  def check_access
+    authenticate_or_request_with_http_basic do |user_name, password|
+      # Change these to username and password required
+      user_name == "username" && password == "pass"
+    end
+  end
 end
                                            
