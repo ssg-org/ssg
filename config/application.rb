473ed30f823978e8049a133594a3a939a12cacce
zpasal <zaharije@atlantbh.com>
2012-10-06 15:42:13 +0200

    Load config before initializer - localconfig fix

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,9 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+require "#{Dir.pwd}/lib/config/config.rb"
+Config::Configuration.reload()
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
