2401f181bc260eb7b1d2e41a023bbfc2d3fd4bab
Eljazovic Samir <samir.eljazovic@gmail.com>
2012-10-29 19:59:56 +0100

    Sorting and filterig implemented

--- db/migrate/20120624204447_agregated_votes_views.rb
+++ db/migrate/20120624204447_agregated_votes_views.rb
@@ -3,6 +3,7 @@ class AgregatedVotesViews < ActiveRecord::Migration
     change_table :issues do |t|
     	t.integer :vote_count,  :default => 0
     	t.integer :view_count,  :default => 0
+    	t.integer :comment_count,  :default => 0
     end
   end
 
