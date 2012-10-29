0e9403ebfa4ba29a1c1b8c74fb704223de1bfbee
zpasal <zaharije@atlantbh.com>
2012-10-29 18:09:36 +0100

    Removed custom fonts - default to Arial

--- config/initializers/_configuration.rb
+++
@@ -1,9 +0,0 @@
-require "#{Dir.pwd}/lib/config/config.rb"
-
-config_scope = Rails.env
-
-puts "Starting with configuration (#{config_scope}):"
-
-Config::Configuration.reload(config_scope)
-
-puts Config::Configuration.get_root(config_scope).inspect
--- config/initializers/configuration.rb
+++
@@ -1,3 +0,0 @@
-require "#{Dir.pwd}/lib/config/config.rb"
-
-puts "Starting with #{Rails.env} configuration :"
