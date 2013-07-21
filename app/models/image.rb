19030930c1c2b15df734ee284dbaccaa1d1c59ab
Haris Krajina <haris@atlantbh.com>
2013-07-21 23:24:30 +0200

    Removed mini magick

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,6 +3,6 @@ require 'carrierwave/orm/activerecord'
 class Image < ActiveRecord::Base
   	belongs_to  :issue
 
-  	mount_uploader :image, ImageUploader
+  	#mount_uploader :image, ImageUploader
     
 end
