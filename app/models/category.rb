595e2c861297568eccf4fac8a434593934fe44c2
Haris Krajina <haris@atlantbh.com>
2013-08-01 00:16:07 +0200

    Changes for success

--- app/models/category.rb
+++ app/models/category.rb
@@ -14,11 +14,9 @@ class Category < ActiveRecord::Base
     category = (category_id = params[:category_id]).empty? ? Category.new : Category.find(category_id)
     category.name = params[:category_name]
     category.description = params[:description]
-    category.parent  = params[:parent_category_id] ? Category.find(params[:parent_category_id]) : nil
-    category.color = params[:color]
+    category.parent  = params[:parent_category_id] && params[:parent_category_id] != '0' ? Category.find(params[:parent_category_id]) : nil
+    category.color = params[:color].gsub('#','')
     category.icon = category.icon || DEFAULT_ICON
-    pp '.,...................'
-    pp category
     category.save!
 
     return category_id.empty?
