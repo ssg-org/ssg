63a655d6fd49b9ffd89ca7a104bf99567309a9d6
Haris Krajina <haris@atlantbh.com>
2013-08-25 13:31:51 +0200

    Fixed to use random images seeding

--- lib/seeding.rb
+++ lib/seeding.rb
@@ -15,7 +15,7 @@ class Seeding
 	  image_ids = []
 	  image_paths.each do |path|
 	 	  image = Image.new()
-=begin
+
 	 	  extension = EXTENSIONS.select { |e| path.include? e }
 
 	 	  return if extension.nil? || extension.first.nil?
@@ -33,8 +33,7 @@ class Seeding
 
 
 	    image.image = tempfile
-=end
-	    image.image = File.new(path)
+	    #image.image = File.new(path)
 	    image.save!
 	    image_ids << image.id
 	  end
@@ -65,8 +64,8 @@ class Seeding
 	      Lorem::Base.new('paragraphs', Random.new.rand(1..3)).output,
 	      categories[Random.new.rand(0..categories.length-1)],
 	      cities[Random.new.rand(0..cities.length-1)],
-	      #RandomImages::Images.get(Random.new.rand(1..3))
-	      [images[Random.new.rand(0..images.length-1)]]
+	      RandomImages::Images.get(Random.new.rand(1..3))
+	      #[images[Random.new.rand(0..images.length-1)]]
 	    )
 	  end
 	end
