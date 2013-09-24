ecad6f3d8e8c238d7b5f3badcd5f23abbec60b25
Haris Krajina <haris@atlantbh.com>
2013-09-24 02:22:55 +0200

    Fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, Config::Configuration.get(:fb, :application_id), Config::Configuration.get(:fb, :secret_key), :client_options => {:ssl => {:verify => false }}, :scope => Config::Configuration.get(:fb, :scope), :display => 'popup'
+  #provider :facebook, Config::Configuration.get(:fb, :application_id), Config::Configuration.get(:fb, :secret_key), :client_options => {:ssl => {:verify => false }}, :scope => Config::Configuration.get(:fb, :scope), :display => 'popup'
+  provider :facebook, Config::Configuration.get(:fb, :application_id), Config::Configuration.get(:fb, :secret_key), :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'} }, :scope => Config::Configuration.get(:fb, :scope), :display => 'popup'
   #provider :facebook, Config::Configuration.get(:fb, :application_id), Config::Configuration.get(:fb, :secret_key), :scope => Config::Configuration.get(:fb, :scope), :display => 'popup'
 end
